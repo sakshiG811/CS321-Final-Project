b49b14e4afd7f14aaa3513f6862812a54250df4d
Sakshi <sakshiG811@gmail.com>
2023-04-26 21:20:03 -0400

    first commit

---
+++ Code/AboutApplication.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePainterApplication
+{
+    public class AboutApplication
+    {
+        public static void AboutFeature(Form1 form)
+        {
+            var appName = "Simple Painter";
+            var version = "1.0";
+            var author = "Sakshi Gupta";
+            var info = $@"Simple Painter is a simple drawing application written in C# using Windows Forms.
+
+Version: {version}
+
+Author: {author}
+
+Welcome to Simple Painter - your new go-to drawing app that brings fun and creativity to your fingertips! 
+Unleash your inner artist with our delightful set of features that make digital art a breeze:
+
+- Get artsy with an array of shapes, like ellipses, rectangles, squares, circles, lines, and freeform doodles. 
+Make your creations pop with customizable stroke widths, stroke colors, and fill colors. 
+Feeling bold? Opt for ""Filled"" to add a vibrant touch to your shapes.
+
+- Discover a handy Menu bar for seamless navigation: manage your Files (New, Open, Save, Save As, Exit), 
+dabble with Edit tools (Undo, Redo, Clear All, Select, Delete), and explore View settings (In Browser, XML Format, About).
+
+- Love precision? You'll adore our built-in ruler and guidelines that'll help you craft pixel-perfect designs like a pro.
+ 
+- Set the mood with the perfect background color - a canvas that truly reflects your artistic vision.
+
+- Feeling adventurous? Press the ""Fun"" button and watch as random shapes, colors, and sizes burst onto your screen, 
+adding an element of surprise to your artwork.
+
+Getting started is a piece of cake! Just pick a tool from the toolbar and let your imagination run wild on the canvas. 
+Mix and match shapes, colors, and sizes to craft your very own masterpiece. 
+When you're ready to show off your work, save it in different formats and share it with friends, family, or colleagues.
+
+If you ever need a helping hand or more info about our app, the ""About"" section under the View menu has got you covered. 
+So, grab your virtual paintbrush and dive into the colorful world of Simple Painter - happy drawing!";
+    
+               MessageBox.Show(info, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
---
+++ Code/MenuEventHandler.cs
@@ -0,0 +1,326 @@
+﻿using Svg;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using ExCSS;
+
+namespace SimplePainterApplication
+{
+    public class MenuEventHandler
+	{
+        public static void NewToolStripMenuItem_Click(Form1 form, object sender, EventArgs e)
+		{
+            if (form.Shapes.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Do you want to save changes before creating a new file?", "Save Changes?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    SaveToolStripMenuItem_Click(form, sender, e);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            form.Shapes.Clear();
+            form.RedoStack.Clear();
+            form.DrawingArea.Invalidate();
+        }
+
+        public static void OpenToolStripMenuItem_Click(Form1 form, object sender, EventArgs e)
+        {
+
+            if (form.Shapes.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Do you want to save changes before opening a new file?", "Save Changes?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    SaveToolStripMenuItem_Click(form, sender, e);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Open SVG File",
+                Filter = "SVG Files|*.svg",
+                Multiselect = false
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    SvgDocument svgDoc;
+                    try
+                    {
+                        svgDoc = SvgDocument.Open(openFileDialog.FileName);
+                        Debug.WriteLine("Opening file: " + openFileDialog.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Unable to load the SVG file.\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    List<Shape> shapes = SvgConverter.ToShapes(svgDoc);
+
+                    Bitmap bitmap = new Bitmap(form.DrawingArea.Width, form.DrawingArea.Height);
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.Clear(System.Drawing.Color.White);
+                        foreach (Shape shape in shapes)
+                        {
+                            shape.Draw(g);
+                        }
+                    }
+
+                    form.DrawingArea.Image = bitmap;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        public static void SaveToolStripMenuItem_Click(Form1 form, object sender, EventArgs e)
+        {
+            Debug.WriteLine("Save menu option clicked.");
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "SVG files|*.svg";
+            saveFileDialog.Title = "Save file...";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Debug.WriteLine("Saving file: " + saveFileDialog.FileName);
+
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Shape>));
+                StreamWriter writer = new StreamWriter(saveFileDialog.FileName);
+                serializer.Serialize(writer, form.Shapes);
+                writer.Close();
+            }
+        }
+
+        public static void SaveAsToolStripMenuItem_Click(Form1 form, object sender, EventArgs e)
+        {
+            Debug.WriteLine("Save As menu option clicked.");
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "SVG files|*.svg|PNG files|*.png|JPEG files|*.jpg";
+            saveFileDialog.Title = "Save file as...";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string extension = Path.GetExtension(saveFileDialog.FileName).ToLower();
+                switch (extension)
+                {
+                    case ".svg":
+                        XmlSerializer serializer = new XmlSerializer(typeof(List<Shape>));
+                        StreamWriter writer = new StreamWriter(saveFileDialog.FileName);
+                        serializer.Serialize(writer, form.Shapes);
+                        writer.Close();
+                        break;
+
+                    case ".png":
+                        Bitmap bmp = new Bitmap(form.DrawingArea.Width, form.DrawingArea.Height);
+                        form.DrawingArea.DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                        bmp.Save(saveFileDialog.FileName, ImageFormat.Png);
+                        break;
+
+                    case ".jpg":
+                        Bitmap bmp2 = new Bitmap(form.DrawingArea.Width, form.DrawingArea.Height);
+                        form.DrawingArea.DrawToBitmap(bmp2, new Rectangle(0, 0, bmp2.Width, bmp2.Height));
+                        bmp2.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                        break;
+
+                    default:
+                        MessageBox.Show("Unsupported file extension: " + extension, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+        }
+
+        public static void ExitToolStripMenuItem_Click(Form1 form, object sender, EventArgs e)
+        {
+            if (form.Shapes.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Do you want to save changes before exiting?", "Save Changes?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    SaveToolStripMenuItem_Click(form, sender, e);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            Application.Exit();
+        }
+
+        public static void Undo_Click(Form1 form, object sender, EventArgs e)
+        {
+            if (form.Shapes.Count > 0)
+            {
+                form.RedoStack.Add(form.Shapes[form.Shapes.Count - 1]);
+                form.Shapes.RemoveAt(form.Shapes.Count - 1);
+                form.DrawingArea.Invalidate();
+            }
+        }
+
+        public static void Redo_Click(Form1 form, object sender, EventArgs e)
+        {
+            if (form.RedoStack.Count > 0)
+            {
+                form.Shapes.Add(form.RedoStack[form.RedoStack.Count - 1]);
+                form.RedoStack.RemoveAt(form.RedoStack.Count - 1);
+                form.DrawingArea.Invalidate();
+            }
+        }
+
+        public static void ClearAll_Click(Form1 form, object sender, EventArgs e)
+        {
+            form.Shapes.Clear();
+            form.RedoStack.Clear();
+            form.DrawingArea.Invalidate();
+        }
+
+        public static void Select_Click(Form1 form, object sender, EventArgs e)
+        {
+            if (form.Shapes.Count == 0)
+            {
+                form.SelectModeEnabled = false;
+                return;
+            }
+
+            form.SelectModeEnabled = true;
+            foreach (var shape in form.Shapes)
+            {
+                shape.Selected = false;
+            }
+            form.SelectedShapes.Clear();
+        }
+
+        public static void Delete_Click(Form1 form, object sender, EventArgs e)
+        {
+            if (form.SelectedShapes.Count > 0)
+            {
+                form.Shapes.RemoveAll(shape => shape.Selected);
+                form.SelectedShapes.Clear();
+                form.DrawingArea.Invalidate();
+                form.SelectModeEnabled = false;
+            }
+        }
+
+        public static void InBrowser_Click(Form1 form, object sender, EventArgs e)
+        {
+            string imageFilePath = "image.html";
+            using (StreamWriter sw = new StreamWriter(imageFilePath))
+            {
+                Bitmap bmp = new Bitmap(form.DrawingArea.Width, form.DrawingArea.Height);
+                form.DrawingArea.DrawToBitmap(bmp, form.DrawingArea.ClientRectangle);
+
+                string base64Image = ImageToBase64(bmp);
+                sw.Write($"<html><body><img src=\"{base64Image}\"></body></html>");
+            }
+
+            // Use the "shell:open" command to open the file in the default web browser
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C start \"\" \"{imageFilePath}\"",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            Process.Start(psi);
+        }
+
+        public static string ImageToBase64(Image image)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 string
+                string base64String = Convert.ToBase64String(imageBytes);
+                return $"data:image/png;base64,{base64String}";
+            }
+        }
+
+        public static void XmlFormat_Click(Form1 form, object sender, EventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Shape>));
+            using (FileStream stream = new FileStream("shapes.xml", FileMode.Create))
+            {
+                serializer.Serialize(stream, form.Shapes);
+            }
+
+            string xml = File.ReadAllText("shapes.xml");
+            MessageBox.Show(xml, "Shapes XML");
+        }
+
+        public static void Form1_KeyDown(Form1 form, object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.N:
+                    if (e.Control)
+                    {
+                        SaveToolStripMenuItem_Click(form, sender, e);
+                    }
+                    break;
+
+                case Keys.O:
+                    if (e.Control)
+                    {
+                        SaveToolStripMenuItem_Click(form, sender, e);
+                    }
+                    break;
+
+                case Keys.S:
+                    if (e.Control && !e.Shift)
+                    {
+                        SaveToolStripMenuItem_Click(form, sender, e);
+                    }
+                    else if (e.Control && e.Shift)
+                    {
+                        SaveAsToolStripMenuItem_Click(form, sender, e);
+                    }
+                    break;
+                case Keys.E:
+                    if (e.Control)
+                    {
+                        ExitToolStripMenuItem_Click(form, sender, e);
+                    }
+                    break;
+                case Keys.Z:
+                    if (e.Control && !e.Shift)
+                    {
+                        Undo_Click(form, sender, e);
+                    }
+                    else if (e.Control && e.Shift)
+                    {
+                        Redo_Click(form, sender, e);
+                    }
+                    break;
+                case Keys.D:
+                    if (e.Control)
+                    {
+                        ClearAll_Click(form, sender, e);
+                    }
+                    break;
+            }
+        }
+    }
+}
+	
+
